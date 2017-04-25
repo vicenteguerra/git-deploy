45f4fdf8e6327a69ca461b2274d94612b02ba6b9
Jacob Bearce <jacob@bearce.me>
2017-04-25 14:57:25 -0500

    Improve same config

--- config.sample.php
+++ config.sample.php
@@ -1,8 +1,8 @@
 <?php
-define('TOKEN', 'secret-token'); // This TOKEN put in Payload URL in GitLab or GitHub, Example: http://mydomain.com/deploy.php?token=mytoken
-define('REMOTE_REPOSITORY', 'git@gitlab.com:HelloWorld.git');
-define('DIR','/var/www/vhosts/HelloWordRepo/'); // It's important that the path ends with '/'
-define('BRANCH','refs/heads/master'); // (Gitlab branch Route)
-define('LOGFILE', "log.txt"); // Log File Name
-define('GIT', '/usr/bin/git'); // Unix git command route
-define('AFTER_PULL', ''); // Command to execute after successfully pulling
+define("TOKEN", "secret-token");                                       // The secret token to add as a GitHub or GitLab secret, or otherwise as https://www.example.com/?token=secret-token
+define("REMOTE_REPOSITORY", "git@github.com:username/repository.git"); // The SSH URL to your repository
+define("DIR", "/var/www/vhosts/repository/");                          // The path to your repostiroy; this must begin with a forward slash (/)
+define("BRANCH", "refs/heads/master");                                 // The branch route
+define("LOGFILE", "deploy.log");                                       // The name of the file you want to log to.
+define("GIT", "/usr/bin/git");                                         // The path to the git executable
+define("AFTER_PULL", "");                                              // A command to execute after successfully pulling
