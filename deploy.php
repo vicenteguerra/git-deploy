fc5a9835d9c09ae671b17d363a937eaef1eaa0e5
Jacob Bearce <jacob@bearce.me>
2017-04-25 14:46:34 -0500

    Fix issue which would prevent ever completeing
    - Add comments
    - Extrapolate forbid in to a function

--- deploy.php
+++ deploy.php
@@ -9,30 +9,36 @@ $time    = time();
 date_default_timezone_set("UTC");
 fputs($file, date("d-m-Y (H:i:s)", $time) . "\n");
 
-if (!empty(TOKEN)) {
-    if (isset($_SERVER["HTTP_X_HUB_SIGNATURE"])) {
-        list($algo, $hash) = explode("=", $_SERVER["HTTP_X_HUB_SIGNATURE"], 2) + array("", "");
+// function to forbid access
+function forbid() {
+    header("HTTP/1.0 403 Forbidden");
+    fputs($file, "*** ACCESS DENIED ***" . "\n\n\n");
+    fclose($file);
+    exit;
+}
 
-        if ($hash !== hash_hmac($algo, $content, TOKEN)) {
-            header("HTTP/1.0 403 Forbidden");
-            fputs($file, "Access Denied" . "\n");
-            exit;
-        }
-    } elseif (isset($_SERVER["HTTP_X_GITLAB_TOKEN"])) {
-        if ($_SERVER["HTTP_X_GITLAB_TOKEN"] !== sha1(TOKEN)) {
-            header("HTTP/1.0 403 Forbidden");
-            fputs($file, "Access Denied" . "\n");
-            exit;
-        }
-    } elseif (isset($_GET["token"])) {
-        if ($_GET["token"] !== TOKEN) {
-            header("HTTP/1.0 403 Forbidden");
-            fputs($file, "Access Denied" . "\n");
-            exit;
-        }
+// Check for a GitHub signature
+if (!empty(TOKEN) && isset($_SERVER["HTTP_X_HUB_SIGNATURE"])) {
+    list($algo, $hash) = explode("=", $_SERVER["HTTP_X_HUB_SIGNATURE"], 2) + array("", "");
+
+    if ($hash !== hash_hmac($algo, $content, TOKEN)) {
+        forbid();
     }
+// Check for a GitLab token
+} elseif (!empty(TOKEN) && isset($_SERVER["HTTP_X_GITLAB_TOKEN"])) {
+    if ($_SERVER["HTTP_X_GITLAB_TOKEN"] !== sha1(TOKEN)) {
+        forbid();
+    }
+// Check for a $_GET token
+} elseif (!empty(TOKEN) && isset($_GET["token"])) {
+    if ($_GET["token"] !== TOKEN) {
+        forbid();
+    }
+// if none of the above match, but a token exists, exit
+} elseif (!empty(TOKEN)) {
+    forbid();
 } else {
-    if ($json["ref"] == BRANCH) {
+    if ($json["ref"] === BRANCH) {
         fputs($file, $content . PHP_EOL);
 
         if (file_exists(DIR . ".git") && is_dir(DIR)) {
@@ -53,7 +59,7 @@ if (!empty(TOKEN)) {
                 fputs($file, $e . "\n");
             }
         } else {
-            fputs($file, "DIR Not Found" . "\n");
+            fputs($file, "DIR is not a repository" . "\n");
         }
     } else{
         fputs($file, "Push in: " . $json["ref"] . "\n");
