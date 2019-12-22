86a16d65a5e93eea3c12970499d4d8b99efe6243
Choicky ZHOU <zhoucaiqi@gmail.com>
2019-12-22 11:55:56 +0800

    Update deploy.sample.php

--- deploy.sample.php
+++ deploy.sample.php
@@ -6,7 +6,7 @@ define("BRANCH", "refs/heads/master");                                 // The br
 define("LOGFILE", "deploy.log");                                       // The name of the file you want to log to.
 define("GIT", "/usr/bin/git");                                         // The path to the git executable
 define("MAX_EXECUTION_TIME", 180);                                     // Override for PHP's max_execution_time (may need set in php.ini)
-define("BEFORE_PULL", "");                                             // A command to execute before pulling
+define("BEFORE_PULL", "/usr/bin/git reset --hard @{u}");               // A command to execute before pulling
 define("AFTER_PULL", "");                                              // A command to execute after successfully pulling
 
 require_once("deployer.php");
